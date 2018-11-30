804f4622966c0bbe8302e4ba0e1d295e0e806189
Burke Webster <burke.webster@gmail.com>
2018-11-29 20:40:10 -0600

    Added rspec.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "bundler/setup"
+Bundler.setup
+
+require "zoom_slack"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.filter_run_when_matching :focus
+
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  config.disable_monkey_patching!
+
+  config.warnings = true
+
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = "doc"
+  end
+
+  config.profile_examples = 10
+
+  config.order = :random
+  Kernel.srand config.seed
+end
